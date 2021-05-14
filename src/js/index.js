644d14aa035dd92201ecefee991db064186c1816
신혁준 <67365409+SHJ-9712@users.noreply.github.com>
2021-05-14 22:10:49 +0900

    Create header toggle menu js animation

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,12 @@ const searchInput = document.querySelector('header .searchForm form');
 searchBtn.addEventListener('click', function() {
     if(searchForm.style.height == '147px') {
         searchForm.style.height = '0';
-        setTimeout(function() {
+        setTimeout(() => {
             searchForm.style.display = 'none';
         }, 400);
     } else {
         searchForm.style.display = 'block';
-        setTimeout(function() {
+        setTimeout(() => {
             searchForm.style.height = '147px';
         }, 100);
     }
@@ -20,8 +20,27 @@ searchBtn.addEventListener('click', function() {
 if(desktopMq) {
     searchForm.addEventListener('mouseleave', function() {
         searchForm.style.height = '0';
-        setTimeout(function() {
+        setTimeout(() => {
             searchForm.style.display = 'none';
         }, 400);
     });
 }
+
+// header Mobile, Tablet toggle menu
+const toggleMenu = document.querySelector('.toggleMenu');
+const categories = document.querySelector('.categories');
+toggleMenu.addEventListener('click', function() {
+    if(categories.style.display == 'block') {
+        categories.style.right = '-341px';
+        setTimeout(() => {
+            categories.style.display = 'none';
+        }, 400);
+        toggleMenu.classList.remove('on');
+    } else {
+        categories.style.display = 'block';
+        setTimeout(() => {
+            categories.style.right = '-21px';
+        }, 100);
+        toggleMenu.classList.add('on');
+    }
+});
